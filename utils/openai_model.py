d5e63e3e039b5b315017e9d759b572775c97bf70
ArchitGupta16 <gupta.archit2001@gmail.com>
2026-02-04 23:21:02 +0530

    Added gemini code

--- utils/openai_model.py
+++ utils/openai_model.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import os
 import dotenv
+from google import genai
+from google.genai import types
 
 dotenv.load_dotenv()
 
@@ -17,4 +19,23 @@ def openai_caller(prompt: str) -> str:
         input=prompt
     )
 
+    # client = genai.Client(
+    #     api_key=os.environ.get('OPENAI_API_KEY')
+    # )
+
+    # grounding_tool = types.Tool(
+    #     google_search=types.GoogleSearch()
+    # )
+
+    # config = types.GenerateContentConfig(
+    #     tools=[grounding_tool]
+    # )
+
+    # response = client.models.generate_content(
+    #     model="gemini-3-flash-preview",
+    #     contents="Who won the euro 2024?",
+    #     config=config,
+    # )
+
+    # print(response.text)
     return response.output_text
